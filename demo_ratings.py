5700ed7f8e0afba80097c189faad50383197aba4
alfa <alfa@3726ccd4-a4b5-4bee-b90a-4105a94b7273>
2008-10-17 05:27:01 +0000

    Added PALFA demo cand_ids to demo_ratings.py
    git-svn-id: svn+ssh://borgii/homes/borgii/aarchiba/svn-repository/pulsar-group/rating/trunk/rating@7 3726ccd4-a4b5-4bee-b90a-4105a94b7273

--- demo_ratings.py
+++ demo_ratings.py
@@ -1,14 +1,32 @@
 
-pdm_cand_ids = {
-    7891: "Known pulsar, bright nuller",
-    7922: "Known pulsar, fainter",
-    4441: "New pulsar, 487 ms",
-    4443: "New pulsar, half frequency",
-    21775: "RFI wrapped at nonzero DM",
-    26526: "Narrowband RFI",
-    759: "Just noise, no peak, 250 ms",
-    31164: "Just noise, no peak, 12 ms",
-}
+import config as c
+
+if c.survey == "PALFA":
+    # PALFA pdm_cand_ids
+    pdm_cand_ids = {
+	284989: "Known pulsar, narrow peak",
+        283739: "Known pulsar, two peaks",
+	591295: "Known pulsar, fainter, with burst of RFI",
+        663819: "New MSP, 2.15 ms",
+	157246: "Noise, 3.21 ms",
+        434756: "Known MSP, bright",
+	2802: "RFI wrapped at nonzero DM",
+        368835: "RFI(?), nulling, DM slightly high",
+    }
+elif c.survey == "DRIFT":
+    # GBT DRIFT pdm_cand_ids
+    pdm_cand_ids = {
+	7891: "Known pulsar, bright nuller",
+        7922: "Known pulsar, fainter",
+	4441: "New pulsar, 487 ms",
+        4443: "New pulsar, half frequency",
+	21775: "RFI wrapped at nonzero DM",
+        26526: "Narrowband RFI",
+	759: "Just noise, no peak, 250 ms",
+        31164: "Just noise, no peak, 12 ms",
+    }
+else:
+    pdm_cand_ids = {}
 
 def demo_rating(rating):
     for (n,i) in sorted([(n,i) for (i,n) in pdm_cand_ids.items()]):
